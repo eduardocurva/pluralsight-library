73129ed218a4196ba1917d03578b51fda3900d8a
Eduardo Henrique de Oliveira <eduardocurva@gmail.com>
2016-12-05 14:25:56 +0000

    Adding gulp to project

--- app.js
+++ app.js
@@ -3,8 +3,36 @@ var express = require('express');
 //instance of express
 var app = express();
 
-var port = 5000;
+var port = process.env.PORT || 5000;
 
-app.listen(port,function(err){
-    console.log('running server on port ' + port)
+var bookRouter = require('./src/routes/bookRoutes');
+
+//used by express first
+app.use(express.static('public'));
+app.set('views', './src/views');
+
+app.set('view engine', 'ejs');
+
+app.use('/Books', bookRouter);
+
+app.get('/', function (req, res) {
+    res.render('index', {
+        title: 'Hello from render',
+        navi: [{
+            Link: '/Books',
+            Text: 'Books'
+        }, {
+            Link: '/Authors',
+            Text: 'Authors'
+        }]
+    });
+});
+
+
+app.get('/books', function (req, res) {
+    res.send('Hello books');
+});
+
+app.listen(port, function (err) {
+    console.log('running server on port ' + port);
 });
